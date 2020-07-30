f69fafc5c8854ed56c9eb2a75a4c0d8d7537c25a
navneet <navneet@live.com>
2020-07-30 19:25:20 +0530

    batched writes

--- src/api/compareFunction/index.js
+++ src/api/compareFunction/index.js
@@ -1,30 +1,49 @@
 const {
     firestore,
     realtime
-  } = require("../../../firebase");
+} = require("../../../firebase");
 
-  var stringSimilarity = require('string-similarity');
+var stringSimilarity = require('string-similarity');
 
 module.exports = {
     fun: async (req, res) => {
         // let datafeild = await firestore.collection('admin-profiles').get();
-        const { uid, plateCandidates ,vid} = req.body;
-        const getVehicle=await (await realtime.database().ref('vehicles').child(uid).child(vid).once('value')).val();
-        
-        if(getVehicle==null){
+        const { uid, plateCandidates, vid } = req.body;
+        const getVehicle = await (await realtime.database().ref('vehicles').child(uid).child(vid).once('value')).val();
+
+        if (getVehicle == null) {
             res.status(400).send("No such vehicle exist");
         }
-        if(getVehicle.status!=="READY")
-        {
+        if (getVehicle.status !== "READY") {
             res.status(400).send("Vehicle not ready");
         }
-        const registerNo= getVehicle.regno.split(" ").join("");
+        const registerNo = getVehicle.regno.split(" ").join("");
         console.log(registerNo);
+
         var matches = stringSimilarity.findBestMatch(registerNo, plateCandidates);
         console.log(matches);
+
+
+        var updates = {};
+        var newKey = realtime.database().ref('logs').child("-MAiiqWnIcXWGBvQbHXc").push().key;
+
+        updates['/logs/' + "-MAiiqWnIcXWGBvQbHXc" + '/' + newKey] = {
+            'vregno': registerNo,
+            'entryts': Date.now(),
+            'status': 'ENTERED',
+        };
+        updates['/vehicles/' + uid + '/' + vid + '/status'] = "PENDING";
+        updates['/vehicles/' + uid + '/' + vid + '/token'] = "adbuadbewyidvaydva";
+        updates['/vehicles/' + uid + '/' + vid + '/amount'] = 5000;
+        updates['/vehicles/' + uid + '/' + vid + '/locID'] = "-MAiiqWnIcXWGBvQbHXc";
+        updates['/vehicles/' + uid + '/' + vid + '/locName'] = "ABC";
+        updates['/vehicles/' + uid + '/' + vid + '/logID'] = newKey;
+
+        realtime.database().ref().update(updates);
+
         res.status(200).send({
-            "UId" : uid,
-            "Plate Candidate" : plateCandidates
+            "UId": uid,
+            "Plate Candidate": plateCandidates
         });
     }
 }
